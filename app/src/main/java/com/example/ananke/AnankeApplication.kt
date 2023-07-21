e91a2381801762d23e70e629e8b15c58749319c4
Carkzis <marc.jowett@gmail.com>
2023-07-21 12:00:49 +0100

    Add timber with buildconfig enabled to allow usage

--- app/src/main/java/com/example/ananke/AnankeApplication.kt
+++ app/src/main/java/com/example/ananke/AnankeApplication.kt
@@ -2,10 +2,16 @@ package com.example.ananke
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber.*
+import timber.log.Timber.Forest.plant
+
 
 @HiltAndroidApp
 class AnankeApplication : Application() {
     override fun onCreate() {
+        if (BuildConfig.DEBUG) {
+            plant(DebugTree())
+        }
         super.onCreate()
     }
 }
