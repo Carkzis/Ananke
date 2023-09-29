e47f51984b1e80cb8f06b08c1db1ff0cf467e106
Carkzis <marc.jowett@gmail.com>
2023-09-29 10:14:27 +0100

    Remove todos from DummyGameRepository for tidiness

--- app/src/test/java/com/carkzis/ananke/testdoubles/DummyGameRepository.kt
+++ app/src/test/java/com/carkzis/ananke/testdoubles/DummyGameRepository.kt
@@ -4,13 +4,9 @@ import com.carkzis.ananke.data.Game
 import com.carkzis.ananke.data.GameRepository
 import com.carkzis.ananke.data.NewGame
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 class DummyGameRepository : GameRepository {
-    override fun getGames(): Flow<List<Game>> {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun addNewGame(newGame: NewGame) {
-        TODO("Not yet implemented")
-    }
+    override fun getGames(): Flow<List<Game>> = flow {}
+    override suspend fun addNewGame(newGame: NewGame) {}
 }
