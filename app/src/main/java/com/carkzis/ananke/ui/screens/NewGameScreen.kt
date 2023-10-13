1bc8f6fde4946aa5a3223d9ed0b307efce40dc7a
Carkzis <marc.jowett@gmail.com>
2023-10-13 11:13:00 +0100

    Remove underline of text field and add container color

--- app/src/main/java/com/carkzis/ananke/ui/screens/NewGameScreen.kt
+++ app/src/main/java/com/carkzis/ananke/ui/screens/NewGameScreen.kt
@@ -1,15 +1,19 @@
 package com.carkzis.ananke.ui.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.tooling.preview.Preview
@@ -102,6 +106,13 @@ private fun AnankeTextField(modifier: Modifier = Modifier, value: String, onValu
         value = value,
         onValueChange = onValueChange,
         shape = RoundedCornerShape(16.dp),
+        colors = TextFieldDefaults.colors(
+            focusedContainerColor = Color.Green,
+            unfocusedContainerColor = Color.Green,
+            focusedIndicatorColor = Color.Transparent,
+            disabledIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
+        ),
         modifier = modifier
             .fillMaxWidth()
             .padding(16.dp)
