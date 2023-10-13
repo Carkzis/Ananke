480d66d7183aaaffbefff588c925365cc38fcb66
Carkzis <marc.jowett@gmail.com>
2023-10-13 10:03:02 +0100

    Extract text field composable for new game screen and add preview

--- app/src/main/java/com/carkzis/ananke/ui/screens/NewGameScreen.kt
+++ app/src/main/java/com/carkzis/ananke/ui/screens/NewGameScreen.kt
@@ -1,7 +1,9 @@
 package com.carkzis.ananke.ui.screens
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -10,12 +12,14 @@ import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.carkzis.ananke.data.NewGame
 import com.carkzis.ananke.navigation.GameDestination
 import com.carkzis.ananke.ui.components.AnankeButton
 import com.carkzis.ananke.ui.components.AnankeText
+import com.carkzis.ananke.ui.theme.AnankeTheme
 import kotlinx.coroutines.flow.Flow
 
 @OptIn(ExperimentalComposeUiApi::class)
@@ -26,65 +30,93 @@ fun NewGameScreen(
     viewModel: NewGameViewModel = hiltViewModel(),
     onShowSnackbar: suspend (String) -> Boolean
 ) {
-    AnankeText(
-        text = "New Game",
+
+    Column {
+        AnankeText(
+            text = "New Game",
+            modifier = modifier
+                .padding(8.dp)
+                .testTag("${GameDestination.NEW}-title")
+        )
+
+        val keyboardController = LocalSoftwareKeyboardController.current
+        LaunchedEffect(Unit) {
+            viewModel.message.collect {
+                keyboardController?.hide()
+                onShowSnackbar(it)
+            }
+        }
+
+        LaunchedEffect(Unit) {
+            viewModel.addGameSuccessEvent.collect { wasSuccessful ->
+                if (wasSuccessful) onAddGameClick()
+            }
+        }
+
+        Column(modifier = Modifier) {
+
+            AnankeTextField(
+                modifier = modifier,
+                value = viewModel.gameTitle,
+                onValueChange = viewModel::updateGameTitle,
+                testTag = "${GameDestination.NEW}-game-title"
+            )
+
+            AnankeTextField(
+                modifier = modifier,
+                value = viewModel.gameDescription,
+                onValueChange = viewModel::updateGameDescription,
+                testTag = "${GameDestination.NEW}-game-description"
+            )
+
+            AnankeButton(onClick = {
+                viewModel.addNewGame(NewGame(viewModel.gameTitle, viewModel.gameDescription))
+            }) {
+                AnankeText(
+                    text = "Add Game",
+                    modifier = modifier
+                        .padding(8.dp)
+                        .testTag("${GameDestination.NEW}-addnewgame-button")
+                )
+            }
+
+            // Test button.
+            AnankeButton(onClick = {
+                viewModel.addNewGame(NewGame("Marc's Game", "It is indescribable."))
+                onAddGameClick()
+            }) {
+                AnankeText(
+                    text = "Add Dummy Game",
+                    modifier = modifier
+                        .padding(8.dp)
+                        .testTag("${GameDestination.NEW}-addnewgame-button-dummy")
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun AnankeTextField(modifier: Modifier = Modifier, value: String, onValueChange: (String) -> Unit, testTag: String) {
+    TextField(
+        value = value,
+        onValueChange = onValueChange,
+        shape = RoundedCornerShape(16.dp),
         modifier = modifier
-            .padding(8.dp)
-            .testTag("${GameDestination.NEW}-title")
+            .fillMaxWidth()
+            .padding(16.dp)
+            .testTag(testTag)
     )
+}
 
-    val keyboardController = LocalSoftwareKeyboardController.current
-    LaunchedEffect(Unit) {
-        viewModel.message.collect {
-            keyboardController?.hide()
-            onShowSnackbar(it)
-        }
-    }
-
-    LaunchedEffect(Unit) {
-        viewModel.addGameSuccessEvent.collect { wasSuccessful ->
-            if (wasSuccessful) onAddGameClick()
-        }
-    }
-
-    Column(modifier = Modifier) {
-
-        TextField(
-            value = viewModel.gameTitle,
-            onValueChange = viewModel::updateGameTitle,
-            modifier = modifier
-                .testTag("${GameDestination.NEW}-game-title")
+@Preview
+@Composable
+private fun NewGameTitleTextViewPreview() {
+    AnankeTheme {"${GameDestination.NEW}-game-title"
+        AnankeTextField(
+            value = "A Game Title",
+            onValueChange = {},
+            testTag = "${GameDestination.NEW}-game-title"
         )
-
-        TextField(
-            value = viewModel.gameDescription,
-            onValueChange = viewModel::updateGameDescription,
-            modifier = modifier
-                .testTag("${GameDestination.NEW}-game-description")
-        )
-
-        AnankeButton(onClick = {
-            viewModel.addNewGame(NewGame(viewModel.gameTitle, viewModel.gameDescription))
-        }) {
-            AnankeText(
-                text = "Add Game",
-                modifier = modifier
-                    .padding(8.dp)
-                    .testTag("${GameDestination.NEW}-addnewgame-button")
-            )
-        }
-
-        // Test button.
-        AnankeButton(onClick = {
-            viewModel.addNewGame(NewGame("Marc's Game", "It is indescribable."))
-            onAddGameClick()
-        }) {
-            AnankeText(
-                text = "Add Dummy Game",
-                modifier = modifier
-                    .padding(8.dp)
-                    .testTag("${GameDestination.NEW}-addnewgame-button-dummy")
-            )
-        }
     }
 }
