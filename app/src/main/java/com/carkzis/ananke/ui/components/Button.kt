9256908495d6fb9867ed2d305e52eefb3fe041f7
Carkzis <marc.jowett@gmail.com>
2023-11-24 12:10:11 +0000

    Add button preview

--- app/src/main/java/com/carkzis/ananke/ui/components/Button.kt
+++ app/src/main/java/com/carkzis/ananke/ui/components/Button.kt
@@ -5,7 +5,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.carkzis.ananke.navigation.GameDestination
 
 @Composable
 fun AnankeButton(onClick: () -> Unit, modifier: Modifier = Modifier, content: @Composable RowScope.() -> Unit) {
@@ -14,4 +17,14 @@ fun AnankeButton(onClick: () -> Unit, modifier: Modifier = Modifier, content: @C
         modifier = modifier.padding(8.dp),
         content = content
     )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AnankeButtonPreview() {
+    AnankeButton(onClick = {}) {
+        AnankeText(
+            text = "A button!"
+        )
+    }
 }
