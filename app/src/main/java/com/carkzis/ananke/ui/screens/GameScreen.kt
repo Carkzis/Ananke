487c44452ba048ce1d20029f62bc0965c16f19d1
Carkzis <marc.jowett@gmail.com>
2023-12-22 14:04:43 +0000

    Reorganise game card and refactor

--- app/src/main/java/com/carkzis/ananke/ui/screens/GameScreen.kt
+++ app/src/main/java/com/carkzis/ananke/ui/screens/GameScreen.kt
@@ -2,9 +2,10 @@ package com.carkzis.ananke.ui.screens
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -18,13 +19,11 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -59,7 +58,7 @@ fun GameScreen(
 
         games.value.forEach { game ->
             item(key = game.id) {
-                GameItem(modifier.testTag("${GameDestination.HOME}-gameitem"), game)
+                GameCard(modifier.testTag("${GameDestination.HOME}-gameitem"), game)
             }
         }
 
@@ -76,53 +75,82 @@ fun GameScreen(
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-private fun GameItem(
+private fun GameCard(
     modifier: Modifier,
     game: Game
 ) {
     Card(
-        modifier = modifier.padding(4.dp),
+        modifier = modifier.padding(4.dp).testTag("${GameDestination.HOME}-gameCard"),
         shape = RoundedCornerShape(16.dp),
-        onClick = {}
     ) {
         Box(modifier = modifier.padding(16.dp)) {
             Column {
-                AnankeText(text = game.name, textStyle = Typography.titleMedium)
-                Spacer(modifier = modifier.height(8.dp))
-                Divider(color = MaterialTheme.colorScheme.onSurface, thickness = 1.dp)
-                Spacer(modifier = modifier.height(8.dp))
+                GameCardTitle(game, modifier)
 
                 Row {
-                    Column(modifier = Modifier.weight(1f)) {
-                        AnankeText(text = game.description, textAlign = TextAlign.Start, modifier = modifier.align(Alignment.Start).height(96.dp))
-                        Spacer(modifier = modifier.height(8.dp))
-                        AnankeText(text = "Players: 0", textAlign = TextAlign.Start, modifier = modifier.align(Alignment.Start), textStyle = Typography.bodySmall)
-                    }
-
-                    IconButton(onClick = {}) {
-                        Icon(
-                            imageVector = Icons.Filled.TransitEnterexit,
-                            contentDescription = null
-                        )
-                    }
+                    GameCardDescriptionText(game, modifier)
+                    GameCardEnterButton(modifier)
                 }
+
+                GameCardMetadata(modifier)
             }
         }
     }
 }
 
+@Composable
+private fun GameCardTitle(game: Game, modifier: Modifier) {
+    AnankeText(text = game.name, textStyle = Typography.titleMedium)
+    GameCardDivider(modifier)
+}
+
+@Composable
+private fun GameCardDivider(modifier: Modifier) {
+    Spacer(modifier = modifier.height(8.dp))
+    Divider(color = MaterialTheme.colorScheme.onSurface, thickness = 1.dp)
+    Spacer(modifier = modifier.height(8.dp))
+}
+
+@Composable
+private fun RowScope.GameCardDescriptionText(game: Game, modifier: Modifier) {
+    AnankeText(
+        text = game.description, textAlign = TextAlign.Start, modifier = modifier
+            .height(96.dp).weight(1f)
+    )
+}
+
+@Composable
+private fun RowScope.GameCardEnterButton(modifier: Modifier) {
+    IconButton(onClick = {}, modifier = modifier.align(CenterVertically)) {
+        Icon(
+            imageVector = Icons.Filled.TransitEnterexit,
+            contentDescription = null
+        )
+    }
+}
+
+@Composable
+private fun ColumnScope.GameCardMetadata(modifier: Modifier) {
+    GameCardDivider(modifier)
+    AnankeText(
+        text = "Players: 0",
+        textAlign = TextAlign.Start,
+        modifier = modifier.align(Alignment.Start),
+        textStyle = Typography.bodySmall
+    )
+}
+
 @Preview
 @Composable
-private fun GameItemPreview() {
+private fun GameCardPreview() {
     AnankeTheme {
-        GameItem(
+        GameCard(
             modifier = Modifier,
             game = Game(
                 id = "",
                 name = "The Game",
-                description = "This is a game. This is a game. This is a game. This is a game. This is a game.",
+                description = "This is a game.",
             )
         )
     }
