ae601e68701caaf59b86eb944c0af15f6cf71385
Carkzis <marc.jowett@gmail.com>
2024-04-12 14:48:03 +0100

    Tidy UI for team and you screens and add previews for each screen

--- app/src/main/java/com/carkzis/ananke/ui/screens/TeamScreen.kt
+++ app/src/main/java/com/carkzis/ananke/ui/screens/TeamScreen.kt
@@ -1,30 +1,49 @@
 package com.carkzis.ananke.ui.screens
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.carkzis.ananke.data.CurrentGame
 import com.carkzis.ananke.navigation.AnankeDestination
-import com.carkzis.ananke.navigation.GameDestination
 import com.carkzis.ananke.ui.components.AnankeText
+import com.carkzis.ananke.ui.theme.AnankeTheme
 
 @Composable
 fun TeamScreen(currentGame: CurrentGame, modifier: Modifier = Modifier) {
-    AnankeText(
-        text = "Team",
-        modifier = modifier
-            .padding(8.dp)
-            .testTag("${AnankeDestination.TEAM}-title")
-    )
+    Column {
+        AnankeText(
+            text = "Team",
+            modifier = modifier
+                .padding(8.dp)
+                .testTag("${AnankeDestination.TEAM}-title"),
+            textStyle = MaterialTheme.typography.headlineMedium
+        )
 
-    AnankeText(
-        text = currentGame.name,
-        modifier = modifier
-            .padding(8.dp)
-            .testTag("${AnankeDestination.TEAM}-current-game"),
-        textStyle = MaterialTheme.typography.headlineMedium
-    )
+        AnankeText(
+            text = currentGame.name,
+            modifier = modifier
+                .padding(8.dp)
+                .testTag("${AnankeDestination.TEAM}-current-game"),
+            textStyle = MaterialTheme.typography.headlineSmall
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun TeamScreenPreview() {
+    AnankeTheme {
+        TeamScreen(
+            currentGame = CurrentGame(
+                id = "1",
+                name = "Preview Game",
+                description = "This is not a real game."
+            )
+        )
+    }
 }
--- app/src/main/java/com/carkzis/ananke/ui/screens/YouScreen.kt
+++ app/src/main/java/com/carkzis/ananke/ui/screens/YouScreen.kt
@@ -1,29 +1,49 @@
 package com.carkzis.ananke.ui.screens
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.carkzis.ananke.data.CurrentGame
 import com.carkzis.ananke.navigation.AnankeDestination
 import com.carkzis.ananke.ui.components.AnankeText
+import com.carkzis.ananke.ui.theme.AnankeTheme
 
 @Composable
 fun YouScreen(currentGame: CurrentGame, modifier: Modifier = Modifier) {
-    AnankeText(
-        text = "You",
-        modifier = modifier
-            .padding(8.dp)
-            .testTag("${AnankeDestination.YOU}-title")
-    )
+    Column {
+        AnankeText(
+            text = "You",
+            modifier = modifier
+                .padding(8.dp)
+                .testTag("${AnankeDestination.YOU}-title"),
+            textStyle = MaterialTheme.typography.headlineMedium
+        )
 
-    AnankeText(
-        text = currentGame.name,
-        modifier = modifier
-            .padding(8.dp)
-            .testTag("${AnankeDestination.YOU}-current-game"),
-        textStyle = MaterialTheme.typography.headlineMedium
-    )
+        AnankeText(
+            text = currentGame.name,
+            modifier = modifier
+                .padding(8.dp)
+                .testTag("${AnankeDestination.YOU}-current-game"),
+            textStyle = MaterialTheme.typography.headlineSmall
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun YouScreenPreview() {
+    AnankeTheme {
+        YouScreen(
+            currentGame = CurrentGame(
+                id = "1",
+                name = "Preview Game",
+                description = "This is not a real game."
+            )
+        )
+    }
 }
