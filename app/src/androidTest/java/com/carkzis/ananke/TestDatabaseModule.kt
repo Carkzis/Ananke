927d2a3feb6e66f5e9700eb222a6956e142ca21e
Carkzis <marc.jowett@gmail.com>
2024-05-10 14:57:51 +0100

    Use in memory databse in android tests using test module DI to fix navigation tests

---
+++ app/src/androidTest/java/com/carkzis/ananke/TestDatabaseModule.kt
@@ -0,0 +1,25 @@
+package com.carkzis.ananke
+
+import android.content.Context
+import androidx.room.Room
+import com.carkzis.ananke.data.AnankeDatabase
+import com.carkzis.ananke.di.DatabaseModule
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import dagger.hilt.testing.TestInstallIn
+import javax.inject.Singleton
+
+@Module
+@TestInstallIn(
+    components = [SingletonComponent::class],
+    replaces = [DatabaseModule::class]
+)
+object TestDatabaseModule {
+    @Singleton
+    @Provides
+    fun providesInMemoryDatabase(@ApplicationContext context: Context): AnankeDatabase {
+        return Room.inMemoryDatabaseBuilder(context, AnankeDatabase::class.java).build()
+    }
+}
