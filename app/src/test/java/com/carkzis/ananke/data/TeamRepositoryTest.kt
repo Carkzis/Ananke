8cda3b72c98ae6d26d3463e32a3c6ec302ad5bcc
Carkzis <marc.jowett@gmail.com>
2025-12-19 12:39:00 +0000

    Add team repository tests for updating the team member limit

--- app/src/test/java/com/carkzis/ananke/data/TeamRepositoryTest.kt
+++ app/src/test/java/com/carkzis/ananke/data/TeamRepositoryTest.kt
@@ -161,6 +161,21 @@ class TeamRepositoryTest {
         assertEquals(listOf(secondTeamMember), remainingTeamMembers)
     }
 
+    @Test
+    fun `repository has default team member limit`() = runTest {
+        val actualTeamConfiguration = teamRepository.teamConfiguration.first()
+        assertEquals(DEFAULT_TEAM_SIZE, actualTeamConfiguration.teamMemberLimit)
+    }
+
+    @Test
+    fun `repository allows updating team configuration`() = runTest {
+        val newTeamMemberLimit = 7
+        teamRepository.updateTeamConfiguration(TeamConfiguration(newTeamMemberLimit))
+
+        val actualTeamConfiguration = teamRepository.teamConfiguration.first()
+        assertEquals(newTeamMemberLimit, actualTeamConfiguration.teamMemberLimit)
+    }
+
     private suspend fun getUserEntitiesAsDomainObjects(id: Long) =
         teamDao.getTeamMembersForGame(id)
             .first()
